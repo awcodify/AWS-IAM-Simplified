06f59fdf80cc7d31826926e16161cfdeb4ccf2ff
awcodify <awcodify@gmail.com>
2025-11-03 13:24:15 +0700

    refactor: consolidate risk analyzer exports into a single file

--- src/lib/risk-analyzer-new.ts
+++
@@ -1,4 +0,0 @@
-// Re-export the refactored risk analyzer components
-export { IAMRiskAnalyzer, riskAnalyzer } from './risk-analyzers/index';
-export { PolicyAnalyzer } from './risk-analyzers/policy-analyzer';
-export { RiskCalculator } from './risk-analyzers/risk-calculator';
